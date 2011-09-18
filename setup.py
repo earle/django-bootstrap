96f8c846085eb0fbad9cbcad2734869e8e199b80
James Addison <github@scottisheyes.com>
2011-09-17 17:38:06 -0700

    Reverting my 'fix'.  Useless.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ package_data = dict(
 )
 
 setup(
-    name = 'django_bootstrap',
+    name = 'django-bootstrap',
     description = 'Twitter Bootstrap ready Django forms.',
     long_description=read('README.mkd'),
     author='Earle Ady',
